21befc98a4e28edd5b6bc1fcd3e43e005575a477
Sean Perryman <se@nperryman.com>
2015-12-04 22:55:43 -0800

    Added some control routes

--- home-control.py
+++ home-control.py
@@ -1,10 +1,27 @@
 from flask import Flask
 app = Flask(__name__)
 
+# Default Route
+@app.route("/")
+def main():
+    return "Welcome!"
+
+# Projector On
 @app.route("/projOn")
 def projOn():
     execfile("power_on_projector.py")
-    
+ 
+# Projector Off   
 @app.route("/projOff")
 def projOff():
     execfile("power_off_projector.py")
+    
+# Power on receiver
+@app.route("/recvOn")
+def recvOn():
+    execfile("receiver_on.py")
+
+# Receiver Source - BluRay
+@app.route("/recvSrcBD")
+def recvSrcBD():
+    execfile("receiver_src_bd.py")
